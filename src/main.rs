9d4744fa09305520f1248abea3ab338e0d1f8432
Steve Frampton <40193522+FarmingtonS9@users.noreply.github.com>
2023-05-08 18:09:58 +1000

    v0.2
    - Added checking for external dimension (width and height) tolerances
    - Added unit tests for checking external dimensions

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,6 @@ fn main() {
     println!("Specific version is AS/NZS 1163:2009");
 
     let reference_shs = SHS::new(100.).length(8000.).gauge(5.).build();
-    let shs_product = SHS::new(100.3)
-        .height(98.8)
-        .length(8110.2)
-        .gauge(4.82)
-        .build();
-
-    dbg!(&reference_shs);
-    dbg!(&shs_product);
 }
 
 #[derive(Debug)]
@@ -33,7 +25,101 @@ impl SHS {
         }
     }
 
-    fn check_external_dimensions(&self) {}
+    //TODO: Rethink logic
+    fn check_external_dimensions(&self, reference_width: &f32, reference_height: &f32) -> bool {
+        let allowed_tolerance_width = 0.01 * reference_width;
+        let allowed_tolerance_height = 0.01 * reference_height;
+
+        println!(
+            "Allowed tolerance for width: {}mm and height: {}mm",
+            allowed_tolerance_width, allowed_tolerance_height
+        );
+
+        let is_width_tolerance_ok = self.check_width_tolerance(*reference_width);
+        let is_height_tolerance_ok = self.check_height_tolerance(*reference_height);
+
+        dbg!(&is_width_tolerance_ok);
+        dbg!(&is_height_tolerance_ok);
+
+        if is_width_tolerance_ok == true && is_height_tolerance_ok == true {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn check_width_tolerance(&self, reference_width: f32) -> bool {
+        let min_tolerance = 0.5;
+        let allowed_tolerance_width = 0.01 * reference_width;
+        let width_difference = self.width - reference_width;
+
+        //Checking for tolerances of width
+        if -min_tolerance < width_difference && min_tolerance > width_difference {
+            println!(
+                "Failed width within {}mm - {}mm of tolerance. Calculated result {}",
+                -min_tolerance, min_tolerance, width_difference
+            );
+            false
+        } else {
+            println!("Minimum width tolerance: Passed!");
+            if -allowed_tolerance_width <= width_difference
+                && allowed_tolerance_width >= width_difference
+            {
+                println!("Required width tolerance: Passed!");
+                true
+            } else {
+                println!("Required width tolerance: Failed!");
+                false
+            }
+        }
+    }
+
+    fn check_height_tolerance(&self, reference_height: f32) -> bool {
+        let min_tolerance = 0.5;
+        let allowed_tolerance_height = 0.01 * reference_height;
+        let height_difference = self.height - reference_height;
+
+        //Check tolerance for height
+        if -min_tolerance < height_difference && min_tolerance > height_difference {
+            println!(
+                "Failed height within {}mm - {}mm of tolerance. Calculated result {}",
+                -min_tolerance,
+                min_tolerance,
+                self.height - reference_height
+            );
+            false
+        } else {
+            println!("Minimum tolerance: Passed!");
+            if -allowed_tolerance_height <= self.height - reference_height
+                && allowed_tolerance_height >= self.height - reference_height
+            {
+                println!("Required height tolerance: Passed!");
+                true
+            } else {
+                println!("Required height tolerance: Failed!");
+                false
+            }
+        }
+    }
+
+    fn check_thickness(&self, reference_gauge: f32) -> bool {
+        let allowed_tolerance_gauge = 0.01 * reference_gauge;
+        let gauge_difference = self.gauge - reference_gauge;
+
+        println!(
+            "Allowed tolerance is {}mm - {}mm",
+            -allowed_tolerance_gauge, allowed_tolerance_gauge
+        );
+
+        if -allowed_tolerance_gauge < gauge_difference && allowed_tolerance_gauge > gauge_difference
+        {
+            println!("Required tolerance: Passed!");
+            true
+        } else {
+            println!("Required tolerance: Failed");
+            false
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -66,5 +152,54 @@ impl SHSBuilder {
             gauge: self.gauge,
             length: self.length,
         }
+    }
+}
+
+#[cfg(test)]
+mod shape_and_mass_test {
+    use super::*;
+
+    #[test]
+    fn failed_within_width_tolerance() {
+        let reference_shs = SHS::new(100.).length(8000.).gauge(5.).build();
+        let shs_product_1 = SHS::new(100.).height(100.).length(8000.).gauge(5.).build();
+
+        assert_eq!(
+            shs_product_1.check_external_dimensions(&reference_shs.width, &reference_shs.height),
+            false
+        );
+    }
+
+    #[test]
+    fn passed_test_for_tolerance() {
+        let reference_shs = SHS::new(100.).length(8000.).gauge(5.).build();
+        let shs_product_1 = SHS::new(100.5).height(99.5).length(8000.).gauge(5.).build();
+
+        assert_eq!(
+            shs_product_1.check_external_dimensions(&reference_shs.width, &reference_shs.height),
+            true
+        )
+    }
+
+    #[test]
+    fn failed_width_tolerance() {
+        let reference_shs = SHS::new(100.).length(8000.).gauge(5.).build();
+        let shs_product_1 = SHS::new(98.9).height(100.5).length(8000.).gauge(5.).build();
+
+        assert_eq!(
+            shs_product_1.check_external_dimensions(&reference_shs.width, &reference_shs.height),
+            false
+        );
+    }
+
+    #[test]
+    fn failed_height_tolerance() {
+        let reference_shs = SHS::new(100.).length(8000.).gauge(5.).build();
+        let shs_product_1 = SHS::new(100.).height(101.1).length(8000.).gauge(5.).build();
+
+        assert_eq!(
+            shs_product_1.check_external_dimensions(&reference_shs.width, &reference_shs.height),
+            false
+        );
     }
 }
